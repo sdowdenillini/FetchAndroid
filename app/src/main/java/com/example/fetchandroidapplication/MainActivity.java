9422a80ab878c89d2ed3340f14152971e3b9cbff
sdowdenillini <sdowden2@illinois.edu>
2023-02-06 01:46:49 -0600

    Finished the App!

--- app/src/main/java/com/example/fetchandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/fetchandroidapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,6 +16,9 @@ import java.io.*;
 import java.util.ArrayList;
 
 import android.os.StrictMode;
+
+import java.lang.*;
+
 
 
 
@@ -27,8 +31,10 @@ public class MainActivity extends AppCompatActivity {
     private Button originalTextBtn;
     private TextView textBox;
 
-    private ArrayList<String> originalList = null;
-    private ArrayList<String> listID = null;
+    private ArrayList<String> originalList;
+    private ArrayList<String> listID;
+
+    private ArrayList<String> nameList;
 
 
     @Override
@@ -43,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
 
         listIDBtn = findViewById(R.id.listIDBtn);
         nameBtn = findViewById(R.id.nameBtn);
-        filterBlankBtn = findViewById(R.id.filterBlankBtn);
         originalTextBtn = findViewById(R.id.originalTextBtn);
         textBox = findViewById(R.id.textBox);
         textBox.setMovementMethod(new ScrollingMovementMethod());
@@ -57,22 +62,63 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        /*
         try {
             textBox.setText(getHTML(url));
-            htmlToOriginalList(getHTML(url));
-
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
+        } */
 
         try {
+            htmlToOriginalList(getHTML(url));
             originalListToListID(originalList);
-            for (int i = 0; i < listID.size(); i++) {
-                System.out.println(listID.get(i));
-            }
+            listIDToName(originalList);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        try {
+            listIDBtn.setOnClickListener(new View.OnClickListener() {
+                @SuppressWarnings("checkstyle:FinalParameters")
+                @Override
+                public void onClick(final View view) {
+                    String display = "";
+                    for (int i = 0; i < listID.size(); i++) {
+                        display += listID.get(i) + "\n";
+                    }
+                    textBox.setText(display);
+                }
+            });
+
+            nameBtn.setOnClickListener(new View.OnClickListener() {
+                @SuppressWarnings("checkstyle:FinalParameters")
+                @Override
+                public void onClick(final View view) {
+                    String display = "";
+                    for (int i = 0; i < nameList.size(); i++) {
+                        display += nameList.get(i) + "\n";
+                    }
+                    textBox.setText(display);
+                }
+            });
+
+            originalTextBtn.setOnClickListener(new View.OnClickListener() {
+                @SuppressWarnings("checkstyle:FinalParameters")
+                @Override
+                public void onClick(final View view) {
+                    String display = "";
+                    for (int i = 0; i < originalList.size(); i++) {
+                        display += originalList.get(i) + "\n";
+                    }
+                    textBox.setText(display);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
 
 
     }
@@ -97,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void htmlToOriginalList(String input) {
-        ArrayList<String> arrList = new ArrayList<String>();
+        ArrayList<String> arrList = new ArrayList<>();
         String[] inputArray = input.split("\n");
         for (int i = 1; i < inputArray.length - 1; i++) {
             arrList.add(inputArray[i]);
@@ -106,10 +152,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void originalListToListID(ArrayList<String> input) {
-        ArrayList<String> arrList1 = new ArrayList<String>();
-        ArrayList<String> arrList2 = new ArrayList<String>();
-        ArrayList<String> arrList3 = new ArrayList<String>();
-        ArrayList<String> arrList4 = new ArrayList<String>();
+        ArrayList<String> arrList1 = new ArrayList<>();
+        ArrayList<String> arrList2 = new ArrayList<>();
+        ArrayList<String> arrList3 = new ArrayList<>();
+        ArrayList<String> arrList4 = new ArrayList<>();
 
         for (int i = 0; i < input.size(); i++) {
             String[] line = input.get(i).split(" ");
@@ -126,10 +172,80 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        listID.addAll(arrList1);
-        listID.addAll(arrList2);
-        listID.addAll(arrList3);
-        listID.addAll(arrList4);
+        ArrayList<String> arrList = new ArrayList<>();
+
+        for (int i = 0; i < arrList1.size(); i++) {
+            arrList.add(arrList1.get(i));
+        }
+        for (int i = 0; i < arrList2.size(); i++) {
+            arrList.add(arrList2.get(i));
+        }
+        for (int i = 0; i < arrList3.size(); i++) {
+            arrList.add(arrList3.get(i));
+        }
+        for (int i = 0; i < arrList4.size(); i++) {
+            arrList.add(arrList4.get(i));
+        }
+
+        listID = arrList;
+    }
+
+    private void listIDToName(ArrayList<String> input) {
+        String[] list1 = new String[input.size()];
+        String[] list2 = new String[input.size()];
+        String[] list3 = new String[input.size()];
+        String[] list4 = new String[input.size()];
+
+        for (int i = 0; i < input.size(); i++) {
+            String[] line = input.get(i).split(" ");
+            char num = line[3].charAt(0);
+
+            if (line[5].contains("Item") == false) {
+                continue;
+            }
+
+            if (num == '1') {
+                int position = Integer.parseInt(line[1].substring(0, line[1].length() - 1));
+                list1[position] = input.get(i);
+            }
+            if (num == '2') {
+                int position = Integer.parseInt(line[1].substring(0, line[1].length() - 1));
+                list2[position] = input.get(i);
+            }
+            if (num == '3') {
+                int position = Integer.parseInt(line[1].substring(0, line[1].length() - 1));
+                list3[position] = input.get(i);
+            }
+            if (num == '4') {
+                int position = Integer.parseInt(line[1].substring(0, line[1].length() - 1));
+                list4[position] = input.get(i);
+            }
+        }
+
+        ArrayList<String> arrList = new ArrayList<>();
+
+        for (int i = 0; i < list1.length; i++) {
+            if (list1[i] != null) {
+                arrList.add(list1[i]);
+            }
+        }
+        for (int i = 0; i < list2.length; i++) {
+            if (list2[i] != null) {
+                arrList.add(list2[i]);
+            }
+        }
+        for (int i = 0; i < list3.length; i++) {
+            if (list3[i] != null) {
+                arrList.add(list3[i]);
+            }
+        }
+        for (int i = 0; i < list4.length; i++) {
+            if (list4[i] != null) {
+                arrList.add(list4[i]);
+            }
+        }
+
+        nameList = arrList;
     }
 
 }
