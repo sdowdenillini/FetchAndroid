e22b26df5fa9a92a6831c96416865f6dbe3fd352
sdowdenillini <sdowden2@illinois.edu>
2023-02-06 01:58:34 -0600

    Finished the App!

--- app/src/main/java/com/example/fetchandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/fetchandroidapplication/MainActivity.java
@@ -25,12 +25,13 @@ import java.lang.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Create Buttons
     private Button listIDBtn;
     private Button nameBtn;
-    private Button filterBlankBtn;
     private Button originalTextBtn;
     private TextView textBox;
 
+    //Create ArrayLists to be used later
     private ArrayList<String> originalList;
     private ArrayList<String> listID;
 
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //This is something I had to add because the app would shut down without it.
+        //It also helped a lot with debugging.
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
@@ -53,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         textBox = findViewById(R.id.textBox);
         textBox.setMovementMethod(new ScrollingMovementMethod());
 
-
+        //Code to grab html from website
         URL url = null;
 
         try {
@@ -62,13 +65,7 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        /*
-        try {
-            textBox.setText(getHTML(url));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } */
-
+        //This called all the methods I mad in order to sort the list of items.
         try {
             htmlToOriginalList(getHTML(url));
             originalListToListID(originalList);
@@ -77,6 +74,8 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        //Listeners for the buttons
+        //I put it in a try-catch loop because it was not guaranteed to work unless the lists above were not empty.
         try {
             listIDBtn.setOnClickListener(new View.OnClickListener() {
                 @SuppressWarnings("checkstyle:FinalParameters")
@@ -124,17 +123,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //Method to grab the html from the website and from that I could grab the list of items.
     public static String getHTML(URL url) throws IOException {
         URLConnection urlConnection = url.openConnection();
+        //Had to use a user-agent to retrieve web content
         urlConnection.setRequestProperty("User-Agent",
                 "Mozilla/5.0 " + "(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                         + "(KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
         String input = "";
         String line = null;
 
-        while ((line = in.readLine()) != null) {
+        while ((line = bufferedReader.readLine()) != null) {
+            //This was used to delete the double quotes, so that my string manipulation would be easier later.
             String result = line.replaceAll("\"", "");
             input += result + "\n";
         }
@@ -142,6 +144,7 @@ public class MainActivity extends AppCompatActivity {
         return input;
     }
 
+    //Converted the original input list (minus the double quotes) to a nice ArrayList, in the original order.
     private void htmlToOriginalList(String input) {
         ArrayList<String> arrList = new ArrayList<>();
         String[] inputArray = input.split("\n");
@@ -151,6 +154,7 @@ public class MainActivity extends AppCompatActivity {
         originalList = arrList;
     }
 
+    //Converted the original ordered list to the same list, but ordered by ID.
     private void originalListToListID(ArrayList<String> input) {
         ArrayList<String> arrList1 = new ArrayList<>();
         ArrayList<String> arrList2 = new ArrayList<>();
@@ -190,6 +194,9 @@ public class MainActivity extends AppCompatActivity {
         listID = arrList;
     }
 
+    //Converted the original list to be ordered by name.
+    //I was originally going to go from ID to name, but it was easier to go order by ID and name at the same time.
+    //Although it makes the function longer, it was easier on my end to read when I created it.
     private void listIDToName(ArrayList<String> input) {
         String[] list1 = new String[input.size()];
         String[] list2 = new String[input.size()];
